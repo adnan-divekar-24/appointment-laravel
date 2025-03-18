ef5275d0527894457d193c5bce98c0dca1aa934f
Adnan <adnandivekar55@gmail.com>
2025-03-18 17:18:00 +0530

    navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,28 @@
 
 </head>
 <body>
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('dashboard') }}">Staff Management and Appointment Booking</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('staff.index') }}">Staff</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('appointments.index') }}">Appointments</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('leaves.index') }}">Leaves</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
 <div class="container mt-4">
     @yield('content')
